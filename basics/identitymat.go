4b504d224ae2fb183e80d49c36294858622e97a2
Hariharan R <53928267+harishgr2602@users.noreply.github.com>
2020-11-04 18:11:06 +0530

    Identity matrix by array

--- basics/identitymat.go
+++ basics/identitymat.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	var identityMatrix [3][3]int = [3][3]int{[3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}}
+	
 	//type2 to do identity matrix by array
 	//var identityMatrix [3][3]int
 	//identityMatrix[0]int = [3]int{1, 0, 0}
