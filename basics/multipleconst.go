b353aa7f2411c2e583d3f8671ae793eec7a8e8f2
Hariharan R <53928267+harishgr2602@users.noreply.github.com>
2020-11-04 14:52:59 +0530

    Using multiple constants

---
+++ basics/multipleconst.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const a int = 4
+	const b string = "fool"
+	const c float32 = 3.14
+	const d bool = true
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%v\n", d)
+}
