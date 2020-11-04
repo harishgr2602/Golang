adc18cfaf85b8f00a17b483fe9f33b8ff26dbb7a
Hariharan R <53928267+harishgr2602@users.noreply.github.com>
2020-11-04 18:09:01 +0530

    concating the slices

--- basics/concatslice.go
+++ basics/concatslice.go
@@ -1,18 +1,18 @@
 package main
 
 func main() {
-	//a := []int{}
-	//fmt.Println(a)
-	//fmt.Printf("Length : %v\n", len(a))
-	//fmt.Printf("Capacity : %v\n", cap(a))
-	//adding elements within an empty slice
-	//a = append(a, 1)
-	//fmt.Println(a)
-	//fmt.Printf("Length : %v\n", len(a))
-	//fmt.Printf("Capacity : %v\n", cap(a))
-	//a = append(a, []int{2, 3, 4, 5}...)
-	//fmt.Println(a)
-	//fmt.Printf("Length : %v\n", len(a))
-	//fmt.Printf("Capacity : %v\n", cap(a))
+	a := []int{}
+	fmt.Println(a)
+	fmt.Printf("Length : %v\n", len(a))
+	fmt.Printf("Capacity : %v\n", cap(a))
+	adding elements within an empty slice
+	a = append(a, 1)
+	fmt.Println(a)
+	fmt.Printf("Length : %v\n", len(a))
+	fmt.Printf("Capacity : %v\n", cap(a))
+	a = append(a, []int{2, 3, 4, 5}...)
+	fmt.Println(a)
+	fmt.Printf("Length : %v\n", len(a))
+	fmt.Printf("Capacity : %v\n", cap(a))
 
 }
