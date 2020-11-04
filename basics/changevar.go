a722fbd78f7700cb29c61020bc52d09c8dd8e794
Hariharan R <53928267+harishgr2602@users.noreply.github.com>
2020-11-04 11:17:24 +0530

    Golang basics

---
+++ basics/changevar.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int = 42
+	fmt.Printf("%v,%T\n", i, i)
+
+	var j float32
+	j = float32(i)
+	fmt.Printf("%v,%T\n", j, j)
+
+	var k string
+	k = string(i)
+	fmt.Printf("%v,%T\n", k, k)
+}
