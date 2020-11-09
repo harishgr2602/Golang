9faac402fc4233a536825ee17c7dc72cc4995904
Hariharan R <53928267+harishgr2602@users.noreply.github.com>
2020-11-09 16:38:25 +0530

    golang programs

---
+++ nov_9_practice/switch3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var value int = 2
+
+	switch {
+	case value == 1:
+		fmt.Println("Hello")
+	case value == 2:
+		fmt.Println("Bonjour")
+	case value == 3:
+		fmt.Println("Namstay")
+	default:
+		fmt.Println("Invalid")
+	}
+
+}
