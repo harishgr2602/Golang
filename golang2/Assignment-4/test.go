a5b8b82c7875faf1b968f6eb4735de4d37f4abdc
Hariharan R <53928267+harishgr2602@users.noreply.github.com>
2020-12-15 17:49:24 +0530

    Assignment-4

---
+++ golang2/Assignment-4/test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", process)
+	http.ListenAndServe(":8081", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	fname := r.FormValue("firster")
+	lname := r.FormValue("laster")
+
+	d := struct {
+		First string
+		Last  string
+	}{
+		First: fname,
+		Last:  lname,
+	}
+
+	tpl.ExecuteTemplate(w, "process.gohtml", d)
+}
